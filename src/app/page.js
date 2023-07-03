03b4ce0d796a87cb2320f54207a223ea40e1972c
Jindřich Knedla <jindra.knedla@seznam.cz>
2023-07-03 14:43:15 +0200

    Layout corrections

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r font-dosis">
+    <main>
         <Link
             href={{
                 pathname: "quiz"
